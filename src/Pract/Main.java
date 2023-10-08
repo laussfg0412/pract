bf63c284890ef6073bcc71f026e3dbbe9f465bf1
Laura Saucedo <laurassaga@gmail.com>
2023-10-07 19:34:37 -0500

    cambio en el calculo

--- src/Pract/Main.java
+++ src/Pract/Main.java
@@ -1,17 +1,17 @@
+package Pract;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) { throw 
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader (isr);
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        float hm;
+        HaM hobj = new HaM();
+        System.out.println("Ingrese las horas: ");
+        hm= Float.parseFloat(br.readLine());
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-    }
+
+
 }
