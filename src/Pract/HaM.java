8bf4e7ad9995766f726f5f302553a839bf1b3a2c
Laura Saucedo <laurassaga@gmail.com>
2023-10-07 19:31:02 -0500

    creacion del cuerpo de la clase

---
+++ src/Pract/HaM.java
@@ -0,0 +1,13 @@
+package Pract;
+import java.io.*;
+public class HaM {
+    private float horas;
+public void AsignarDatos(float h)
+    horas = h;
+public CalcularMinutos ()
+    float minutos;
+    minutos = horas/60
+    return minutos;
+public DevolverHoras()
+    return horas;
+}
