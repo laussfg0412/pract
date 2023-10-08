a089cb7c69e877edbccf08d5a46c943e4313050a
Laura Saucedo <laurassaga@gmail.com>
2023-10-07 20:01:27 -0500

    Main finalizado

--- src/Pract/Main.java
+++ src/Pract/Main.java
@@ -1,19 +1,20 @@
 package Pract;
 
+import java.io.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) { throw
+    public static void main(String[] args)throws IOException {
         InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader (isr);
+        BufferedReader br = new BufferedReader(isr);
+        float hs;
 
-        float hm;
         HaM hobj = new HaM();
         System.out.println("Ingrese las horas: ");
-        hm= Float.parseFloat(br.readLine());
-        System.out.println("Lass horas ingresadas fueron: "+hobj.AsignarDatos();)
-        System.Out.println("Las horas en minutos son: "+hobj.);
-
-
-
+        hs = Float.parseFloat(br.readLine());
+        hobj.AsignarDatos(hs);
+        System.out.println("Lass horas ingresadas fueron: " + hobj.DevolverHoras());
+        System.out.println("Las horas en minutos son: " + hobj.CalcularMinutos());
+    }
 }
